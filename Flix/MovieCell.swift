0a808cf5e69934cf5b1033cd5ad6cadc9f8a7d97
Deepen Patel <dipenkpatel3@gmail.com>
2020-09-03 23:46:01 -0700

    added all the files

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,27 @@
+ //
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Deepen Patel on 9/3/20.
+//  Copyright © 2020 rcon. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
