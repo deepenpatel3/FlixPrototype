1b9ae5c463ceceeb4fc85748591624660ba71316
Deepen Patel <dipenkpatel3@gmail.com>
2020-09-11 19:35:12 -0700

    completed unit 2 assignment

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Deepen Patel on 9/11/20.
+//  Copyright © 2020 rcon. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
